f9561143c1730d6c3053411c551e803f61619503
Kegan Maher <kegan.maher@smgov.net>
2014-09-25 16:18:14 -0700

    simplifying js, closes #7

--- js/salarySchedules.js
+++ js/salarySchedules.js
@@ -1,155 +1,139 @@
-function SalaryScheduleApp($target) {
+//knockout view models
 
-    //knockout view models
+function salaryScheduleViewModel() {
+    var self = this;
 
-    function salaryScheduleViewModel() {
-        var self = this;
+    self.BargainingUnits = ko.observableArray().extend({ rateLimit: 50 });
+    self.FiscalYear = ko.observable();
+    self.JobClasses = ko.observableArray().extend({ rateLimit: 50 });
+    self.ReportRunDate = ko.observable();
+    self.TitleFilter = ko.observable();
+    self.BargainingUnitsFilter = ko.observable();
+    self.FilteredResultsCount = ko.observable(-1);
 
-        self.BargainingUnits = ko.observableArray().extend({ rateLimit: 50 });
-        self.FiscalYear = ko.observable();        
-        self.JobClasses = ko.observableArray().extend({ rateLimit: 50 });
-        self.ReportRunDate = ko.observable();
-        self.TitleFilter = ko.observable();
-        self.BargainingUnitsFilter = ko.observable();
-        self.FilteredResultsCount = ko.observable(-1);
+    self.FilteredJobClasses = ko.computed(function () {
+        var title = self.TitleFilter() || "",
+            bu = self.BargainingUnitsFilter() || "";
 
-        self.FilteredJobClasses = ko.computed(function () {
-            var title = self.TitleFilter() || "",
-                bu = self.BargainingUnitsFilter() || "";
+        if (title || bu) {
+            var toKeep = ko.utils.arrayFilter(self.JobClasses(), function (jc) {
+                var keep = true;
 
-            if (title || bu) {
-                var toKeep = ko.utils.arrayFilter(self.JobClasses(), function (jc) {
-                    var keep = true;
-                    
-                    if (title)
-                        keep = keep && (jc.Title().toLowerCase().indexOf(title.toLowerCase()) > -1);
-
-                    if (bu)
-                        keep = keep && (jc.BargainingUnit && jc.BargainingUnit.Code && jc.BargainingUnit.Code() && jc.BargainingUnit.Code().toLowerCase() === bu.toLowerCase());
-
-                    return keep;
-                });
-
-                self.FilteredResultsCount(toKeep.length);
-
-                return toKeep;
-            }
-            else {
-                self.FilteredResultsCount(-1);
-                return self.JobClasses();
-            }
-        });
-        
-        self.FiscalYearLabel = ko.computed(function () {
-            return self.FiscalYear() ? "FY " + self.FiscalYear().ShortSpanCode : "No Fiscal Year";
-        });
-       
-        self.ResultsCountMessage = ko.computed(function () {
-            var count = self.FilteredResultsCount();
-            
-            if (count == 0)
-                return "(no results)";
-            else if (count == 1)
-                return "(1 result)";
-            else if (count > 1)
-                return "(" + count + " results)";
-            else
-                return "";
-        });
-
-        self.FilterByBargainingUnit = function (bu) {
-            self.BargainingUnitsFilter(bu.Code || "");
-        };        
-       
-        self.Initialize = function (data) {
-            self.ReportRunDate(data.ReportRunDate);
-            self.FiscalYear(data.FiscalYear);
-            
-	        self.BargainingUnits([]);
-			
-            $.each(data.BargainingUnits, function (i, e) {
-                self.BargainingUnits.push(e);
-            });
-
-			self.JobClasses([]);
-			
-            $.each(data.JobClasses, function (i, e) {
-                self.JobClasses.push(new jobClassViewModel(e));
-            });
-			
-			self.TitleFilter("");
-			self.BargainingUnitsFilter("");
-        };
-    };
-
-    function jobClassViewModel(jobClassData) {
-        var self = this;
-        var mapping = {
-            "Steps": {
-                create: function (options) {
-                    return new jobClassStepViewModel(options.data);
+                if (title) {
+                    keep = keep && (jc.Title().toLowerCase().indexOf(title.toLowerCase()) > -1);
                 }
+
+                if (bu) {
+                    keep = keep && (jc.BargainingUnit && jc.BargainingUnit.Code && jc.BargainingUnit.Code() && jc.BargainingUnit.Code().toLowerCase() === bu.toLowerCase());
+                }
+
+                return keep;
+            });
+
+            self.FilteredResultsCount(toKeep.length);
+
+            return toKeep;
+        }
+        else {
+            self.FilteredResultsCount(-1);
+            return self.JobClasses();
+        }
+    });
+
+    self.FiscalYearLabel = ko.computed(function () {
+        return self.FiscalYear() ? "FY " + self.FiscalYear().ShortSpanCode : "No Fiscal Year";
+    });
+
+    self.ResultsCountMessage = ko.computed(function () {
+        var count = self.FilteredResultsCount();
+
+        if (count == 0)
+            return "(no results)";
+        else if (count == 1)
+            return "(1 result)";
+        else if (count > 1)
+            return "(" + count + " results)";
+        else
+            return "";
+    });
+
+    self.FilterByBargainingUnit = function (bu) {
+        self.BargainingUnitsFilter(bu.Code || "");
+    };        
+
+    self.Initialize = function (data) {
+        self.ReportRunDate(data.ReportRunDate);
+        self.FiscalYear(data.FiscalYear);
+
+        self.BargainingUnits([]);
+
+        $.each(data.BargainingUnits, function (i, e) {
+            self.BargainingUnits.push(e);
+        });
+
+        self.JobClasses([]);
+
+        $.each(data.JobClasses, function (i, e) {
+            self.JobClasses.push(new jobClassViewModel(e));
+        });
+
+        self.TitleFilter("");
+        self.BargainingUnitsFilter("");
+    };
+};
+
+function jobClassViewModel(jobClassData) {
+    var self = this;
+    var mapping = {
+        "Steps": {
+            create: function (options) {
+                return new jobClassStepViewModel(options.data);
             }
-        };
-
-        ko.mapping.fromJS(jobClassData, mapping, self);
-    };
-
-    function jobClassStepViewModel(jobClassStepData) {
-        var self = this;
-        
-        self.Hourly =  ko.computed(function () {
-           return self.HourlyRate() ? self.HourlyRate().toFixed(2) : "0.00";
-        });
-        
-        self.BiWeekly =  ko.computed(function () {
-           return self.BiWeeklyRate() ? self.BiWeeklyRate().toFixed(2) : "0.00";
-        });
-        
-        self.Monthly =  ko.computed(function () {
-           return self.MonthlyRate() ? self.MonthlyRate().toFixed(2) : "0.00";
-        });
-        
-        self.Annual =  ko.computed(function () {
-           return self.AnnualRate() ? self.AnnualRate().toFixed(2) : "0.00";
-        });
-        
-        ko.mapping.fromJS(jobClassStepData, {}, self);
-    };
-    
-    //helper functions
-    
-    function loadComplete(data, callback) {
-        rebind(data);
-        
-        if (callback)
-            callback();
-            
-        $target.fadeIn();
-    };
-
-    var rebind = function (data) {
-        viewModel.Initialize(data);
-        
-        if (!bound) {
-            ko.applyBindings(viewModel);
-            bound = true;
         }
     };
 
-    //"private" data
+    ko.mapping.fromJS(jobClassData, mapping, self);
+};
 
-    var loadedData = {},
-        viewModel = new salaryScheduleViewModel(),
-        bound = false;
+function jobClassStepViewModel(jobClassStepData) {
+    var self = this;
 
-    //the public API for SalaryScheduleApp
+    self.Hourly =  ko.computed(function () {
+       return self.HourlyRate() ? self.HourlyRate().toFixed(2) : "0.00";
+    });
+
+    self.BiWeekly =  ko.computed(function () {
+       return self.BiWeeklyRate() ? self.BiWeeklyRate().toFixed(2) : "0.00";
+    });
+
+    self.Monthly =  ko.computed(function () {
+       return self.MonthlyRate() ? self.MonthlyRate().toFixed(2) : "0.00";
+    });
+
+    self.Annual =  ko.computed(function () {
+       return self.AnnualRate() ? self.AnnualRate().toFixed(2) : "0.00";
+    });
+
+    ko.mapping.fromJS(jobClassStepData, {}, self);
+};
+
+//document ready
+
+$(function () {
+    var loadedData = {};
+    var viewModel = new salaryScheduleViewModel();
+    var bindingsApplied = false;
+    var $loading = $("<p />").addClass("loading").text("Loading...");
+    var $container = $("#data");
     
-    this.loadScheduleData = function (filePath, callback) {
-        $target.fadeOut();
-
+    $("#explore").on("click", function () {
+        $container.before($loading.clone());
+        $container.hide();
+        
+        var filePath = $("#YearSelect").val();
+        
         if (loadedData[filePath]) {
-            loadComplete(loadedData[filePath], callback);
+            loadComplete(loadedData[filePath]);
         }
         else {
             $.ajax({
@@ -159,32 +143,13 @@ function SalaryScheduleApp($target) {
                 success: function (data) {
                     if (data.d) data = data.d;
                     loadedData[filePath] = data;
-                    loadComplete(data, callback);
+                    loadComplete(data);
                 },
                 error: function (e) {
                     console.log(e);
-                },
+                }
             });
         }
-
-        return false;
-    };
-};
-
-//hooks into the page lifecycle
-
-$(function () {
-    var $dataContainer = $("#data"),
-        $loader = $("#loader"),
-        app = new SalaryScheduleApp($dataContainer);
-    
-    $("#explore").on("click", function (e) {
-        $loader.show();
-        e.preventDefault();
-        var filePath = $("#YearSelect").val();
-        app.loadScheduleData(filePath, function () {
-            window.setTimeout(function () { $loader.hide(); }, 250);            
-        });
     });
 	
 	$("#download").on("click", function(e) {
@@ -196,4 +161,17 @@ $(function () {
     $("td.code a").on("click", function () {
         $(this).parent("tr").addClass("selected");
     });
+    
+    var loadComplete = function(data) {
+        viewModel.Initialize(data);
+        
+         if(!bindingsApplied) {
+            ko.applyBindings(viewModel);
+            bindingsApplied = true;
+        }
+        
+        $(".loading").remove();
+        $container.fadeIn(300);
+    };
+    
 });
